34e8815586caf3c62d655497a43d8e43a550f86c
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-11 23:46:02 +0900

    feat(): APi요청을 위한 controller 구현
    메시지 조회를 APi요청 문 구현함.

--- src/main/java/com/usg/chat/adapter/in/web/ChatApiController.java
+++ src/main/java/com/usg/chat/adapter/in/web/ChatApiController.java
@@ -2,6 +2,7 @@ package com.usg.chat.adapter.in.web;
 
 import com.usg.chat.adapter.in.web.dto.MessageRequest;
 import com.usg.chat.adapter.in.web.dto.Result;
+import com.usg.chat.application.port.in.GetMessageHistoryUseCase;
 import com.usg.chat.application.port.in.SendMessageUseCase;
 import com.usg.chat.domain.Chat;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,11 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class ChatApiController {
     private final SendMessageUseCase sendMessageUseCase;
+    private final GetMessageHistoryUseCase getMessageHistoryUseCase;
 
     @Operation(summary = "메시지 전송 *")
     @PostMapping("api/send")
@@ -26,5 +30,13 @@ public class ChatApiController {
                 .build();
         sendMessageUseCase.sendMessage(chat);
         return ResponseEntity.ok(new Result(request.getMessage() , "메시지 전송 완료"));
+    }
+
+    @Operation(summary = "메시지 조회 *")
+    @GetMapping("api/history")
+    public ResponseEntity<Result<List<Chat>>> getMessageHistory(@RequestParam String senderId,
+                                                        @RequestParam String receiverId){
+        List<Chat> Messages = getMessageHistoryUseCase.getMessageHistory(senderId,receiverId);
+        return ResponseEntity.ok(new Result(Messages));
     }
 }
