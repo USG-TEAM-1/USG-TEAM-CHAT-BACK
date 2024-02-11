ff97d4aa98ab34b1408fd0c68c7e98967db3d25c
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-11 23:44:45 +0900

    feat(): ChatPersistencePort구현
    ChatMessageHistory를 적용 시키기 위한 port 구현

--- src/main/java/com/usg/chat/application/port/out/ChatPersistencePort.java
+++ src/main/java/com/usg/chat/application/port/out/ChatPersistencePort.java
@@ -2,6 +2,9 @@ package com.usg.chat.application.port.out;
 
 import com.usg.chat.domain.Chat;
 
+import java.util.List;
+
 public interface ChatPersistencePort {
     void saveChat(Chat chat);
+    List<Chat> getMessageHistory(String senderId, String receiverId);
 }
