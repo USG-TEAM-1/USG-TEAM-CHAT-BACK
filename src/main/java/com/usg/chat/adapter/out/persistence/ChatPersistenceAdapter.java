eed3983ccd0af5ee895de16e4a3e6e462567a801
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-11 22:51:04 +0900

    feat(): ChatPersistenceAdapter 추가
    나중에 채팅 목록 조회 하는것 구현해야함.

--- src/main/java/com/usg/chat/adapter/out/persistence/ChatPersistenceAdapter.java
+++ src/main/java/com/usg/chat/adapter/out/persistence/ChatPersistenceAdapter.java
@@ -1,4 +1,28 @@
 package com.usg.chat.adapter.out.persistence;
 
-public class ChatPersistenceAdapter {
+import com.usg.chat.adapter.out.persistence.entity.ChatEntity;
+import com.usg.chat.adapter.out.persistence.entity.ChatRepository;
+import com.usg.chat.application.port.out.ChatPersistencePort;
+import com.usg.chat.domain.Chat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ChatPersistenceAdapter implements ChatPersistencePort{
+
+    private final ChatRepository chatRepository;
+
+    @Override
+    public void saveChat(Chat chat){
+        ChatEntity chatEntity = ChatEntity
+                .builder()
+                .message(chat.getMessage())
+                .receiverId(chat.getReceiverId())
+                .senderId(chat.getSenderId())
+                .build();
+
+        chatRepository.save(chatEntity);
+    }
+
 }
