6a6b4bda20dbd36ecfed211008b60ed0672e6cfd
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-11 23:41:09 +0900

    fix(): 파일 이름 리팩토링 및 생성자 추가
    T data만 불러올 수 있는 메시지 방식 구현

--- src/main/java/com/usg/chat/adapter/in/web/dto/Result.java
+++ src/main/java/com/usg/chat/adapter/in/web/dto/Result.java
@@ -7,6 +7,7 @@ public class Result <T>{        //로그 남기기 위한 메시지 파트
     private T data;
     private String message;
 
+    public Result(T data) {this.data = data;}
     public Result(T data, String message){
         this.data = data;
         this.message = message;
