28b7ca4d80c06cc2dfd67e43ecf4c7aa29397699
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-11 22:52:46 +0900

    feat(): chat domain 생성
    도메인 생성.

--- src/main/java/com/usg/chat/domain/Chat.java
+++ src/main/java/com/usg/chat/domain/Chat.java
@@ -1,4 +1,23 @@
 package com.usg.chat.domain;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Chat {
+
+    private String message;     //메시지 내용
+    private String senderId;   //보내는 자 아이디
+    private String receiverId; //받는 자 아이디
+
+    @Builder
+    public Chat(String message, String senderId, String receiverId){
+        this.message = message;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+    }
+
 }
