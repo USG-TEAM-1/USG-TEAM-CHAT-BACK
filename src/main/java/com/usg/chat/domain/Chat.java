2dca0ec76adbcbb0e3eca581756528a2ed33200c
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-14 11:21:02 +0900

    fix(): Chat 도메인 변수값 추가
    채팅 시간 기입을 위한 데이터 추가

--- src/main/java/com/usg/chat/domain/Chat.java
+++ src/main/java/com/usg/chat/domain/Chat.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collector;
+
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Chat {
@@ -12,12 +16,16 @@ public class Chat {
     private String message;     //메시지 내용
     private String senderId;   //보내는 자 아이디
     private String receiverId; //받는 자 아이디
+    private LocalDateTime timestamp; //시간
+
+    //private Long chatroom;  //채팅방
 
     @Builder
-    public Chat(String message, String senderId, String receiverId){
+    public Chat(String message, String senderId, String receiverId, LocalDateTime timestamp/*채팅방 넣는거 추가*/){
         this.message = message;
         this.senderId = senderId;
         this.receiverId = receiverId;
+        this.timestamp = timestamp;
+        //this.chatroom = chatroom;
     }
-
 }
