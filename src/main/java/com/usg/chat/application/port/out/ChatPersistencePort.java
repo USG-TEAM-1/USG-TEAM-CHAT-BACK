98b85a900ee831e66f9db5bab3219dc4892591b1
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-14 11:19:51 +0900

    fix(): Long 값 추가 및 getMessageHistory 명칭 변경
    return 값 받고 이름 너무 길어서 줄임.

--- src/main/java/com/usg/chat/application/port/out/ChatPersistencePort.java
+++ src/main/java/com/usg/chat/application/port/out/ChatPersistencePort.java
@@ -1,10 +1,11 @@
 package com.usg.chat.application.port.out;
 
+
 import com.usg.chat.domain.Chat;
 
 import java.util.List;
 
 public interface ChatPersistencePort {
-    void sendMessage(Chat chat);
-    List<Chat> getMessageHistory(String senderId, String receiverId);
+    Long saveMessage(Chat chat);
+    List<Chat> getMessages(String senderId, String receiverId);
 }
