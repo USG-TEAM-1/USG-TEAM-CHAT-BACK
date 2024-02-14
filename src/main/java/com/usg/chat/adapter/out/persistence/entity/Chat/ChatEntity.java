3adf4510e97ee6e4ca80fd22a2bdfdf2f8ae99d4
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-14 11:08:59 +0900

    feat(): ChatEntity 구현
    채팅 메시지, 보내는사람, 받는사람, 시간 추가.

---
+++ src/main/java/com/usg/chat/adapter/out/persistence/entity/Chat/ChatEntity.java
@@ -0,0 +1,37 @@
+package com.usg.chat.adapter.out.persistence.entity.Chat;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table (name = "chat")
+@Getter
+@NoArgsConstructor
+public class ChatEntity {
+
+    @Id @GeneratedValue
+    @Column(name ="chat_id")
+    private Long id;
+    private String message;     //메시지 내용
+    private String senderId;   //보내는 자 아이디
+    private String receiverId; //받는 자 아이디
+    private LocalDateTime timestamp; //시간
+
+//    @OneToOne
+//    private ChatRoomEntity chatroom;  //채팅방
+
+    @Builder
+    public ChatEntity(String message, String senderId, String receiverId,
+                      LocalDateTime timestamp/*, ChatRoomEntity chatroom*/){
+        this.message = message;
+        this.senderId = senderId;
+        this.receiverId = receiverId;
+        this.timestamp = timestamp;
+        //this.chatroom = chatroom;
+    }
+
+}
