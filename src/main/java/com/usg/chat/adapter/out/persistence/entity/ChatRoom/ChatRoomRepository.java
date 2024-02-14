2ffcdb75385d9962e01c4400d0050a560275e462
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-14 11:11:42 +0900

    feat(): ChatRoomRepository 구현
    추후 채팅방 연동시 필요

---
+++ src/main/java/com/usg/chat/adapter/out/persistence/entity/ChatRoom/ChatRoomRepository.java
@@ -0,0 +1,7 @@
+package com.usg.chat.adapter.out.persistence.entity.ChatRoom;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, Long> {
+
+}
