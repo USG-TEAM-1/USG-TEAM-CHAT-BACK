b7b85375facf2ab513b52860c31f5dfa99f0c301
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-14 11:10:38 +0900

    feat(): ChatRoomApiController 컨트롤러 구현
    추후 채팅방 연동시 필요

---
+++ src/main/java/com/usg/chat/adapter/in/web/ChatRoomApiController.java
@@ -0,0 +1,15 @@
+package com.usg.chat.adapter.in.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class ChatRoomApiController {
+
+
+
+}
