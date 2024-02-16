c289f9f9990bf6e1ace02c715760e3799161ba8e
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-16 18:42:10 +0900

    fix(): ChatRoom 수정
    여기다가 이런식으로 넣으면 안됨.

--- src/main/java/com/usg/chat/domain/ChatRoom.java
+++ src/main/java/com/usg/chat/domain/ChatRoom.java
@@ -1,18 +1,18 @@
 package com.usg.chat.domain;
 
+import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomEntity;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class ChatRoom {
+public class ChatRoom{
     private String senderAndReceiver;
 
+    @Builder
     public ChatRoom(String senderAndReceiver) {
         this.senderAndReceiver = senderAndReceiver;
     }
 
-    public void setSenderAndReceiver(int senderId, int receiverId) {
-        this.senderAndReceiver = senderId + "_" + receiverId;
-    }
 }
