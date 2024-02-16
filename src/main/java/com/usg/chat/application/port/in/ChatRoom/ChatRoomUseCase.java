10d5c49e4415c87ce6e34263b64f005d4f2471c3
nodap22 <sanha0046@naver.com>
2024-02-16 12:44:19 +0900

    feat(): 메서드 생성
    createChatRoom <- 메서드 생성

--- src/main/java/com/usg/chat/application/port/in/ChatRoom/ChatRoomUseCase.java
+++ src/main/java/com/usg/chat/application/port/in/ChatRoom/ChatRoomUseCase.java
@@ -1,4 +1,8 @@
 package com.usg.chat.application.port.in.ChatRoom;
 
 public interface ChatRoomUseCase {
+    void createChatRoom(String senderAndReceiver);
+
+    void createChatRoom(int senderId, int receiverId);
+
 }
