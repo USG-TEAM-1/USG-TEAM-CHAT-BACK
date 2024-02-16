57c2a359d29cb317450b3fc7c82a7d58f74ccaee
nodap22 <sanha0046@naver.com>
2024-02-16 12:39:27 +0900

    feat(): import 추가, 메서드 추가
    Component <- import 추가
    ChatRoomPersistenceAdapter <- 메서드 추가
    createChatRoom <- 메서드 추가

--- src/main/java/com/usg/chat/adapter/out/persistence/ChatRoomPersistenceAdapter.java
+++ src/main/java/com/usg/chat/adapter/out/persistence/ChatRoomPersistenceAdapter.java
@@ -1,6 +1,36 @@
 package com.usg.chat.adapter.out.persistence;
 
-import com.usg.chat.application.port.out.ChatRoomPersistencePort;
+import com.usg.chat.application.port.in.ChatRoom.ChatRoomUseCase;
+import com.usg.chat.domain.ChatRoom;
+import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomEntity;
+import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomRepository;
+import org.springframework.stereotype.Component;
 
-public class ChatRoomPersistenceAdapter implements ChatRoomPersistencePort {
+@Component
+public class ChatRoomPersistenceAdapter implements ChatRoomUseCase {
+    private final ChatRoomRepository chatRoomRepository;
+
+    public ChatRoomPersistenceAdapter(ChatRoomRepository chatRoomRepository) {
+        this.chatRoomRepository = chatRoomRepository;
+    }
+
+    @Override
+    public void createChatRoom(String senderAndReceiver) {
+
+    }
+
+    @Override
+    public void createChatRoom(int senderId, int receiverId) {
+        String senderAndReceiver;
+        if(senderId < receiverId) {
+            senderAndReceiver = senderId + "_" + receiverId;
+        } else {
+            senderAndReceiver = receiverId + "_" + senderId;
+        }
+
+        ChatRoom chatRoom = new ChatRoom(senderAndReceiver);
+        ChatRoomEntity chatRoomEntity = new ChatRoomEntity();
+        chatRoomEntity.setSenderAndReceiver(chatRoom.getSenderAndReceiver());
+        chatRoomRepository.save(chatRoomEntity);
+    }
 }
