efdd4905776c979993424deec6a325b7e85f6b56
nodap22 <sanha0046@naver.com>
2024-02-16 12:35:28 +0900

    feat(): import 추가, 메서드 생성
    RestController,RequestBody,PostMapping <- import 추가
    ChatRoomApiController, createChatRoom <- 메서드 생성

--- src/main/java/com/usg/chat/adapter/in/web/ChatRoomApiController.java
+++ src/main/java/com/usg/chat/adapter/in/web/ChatRoomApiController.java
@@ -1,15 +1,28 @@
 package com.usg.chat.adapter.in.web;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import com.usg.chat.application.port.in.ChatRoom.ChatRoomUseCase;
+import com.usg.chat.application.port.in.ChatRoom.ChatRoomCommand;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-
-@Slf4j
 @RestController
-@RequiredArgsConstructor
 public class ChatRoomApiController {
+    private final ChatRoomUseCase chatRoomUseCase;
 
+    public ChatRoomApiController(ChatRoomUseCase chatRoomUseCase) {
+        this.chatRoomUseCase = chatRoomUseCase;
+    }
 
+    @PostMapping("/chat-rooms")
+    public ResponseEntity<String> createChatRoom(@RequestBody ChatRoomCommand command) {
+        int senderId = command.getSenderId();
+        int receiverId = command.getReceiverId();
+        String senderAndReceiver = (senderId < receiverId) ? senderId + "_" + receiverId : receiverId + "_" + senderId;
 
+        chatRoomUseCase.createChatRoom(senderAndReceiver);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
 }
