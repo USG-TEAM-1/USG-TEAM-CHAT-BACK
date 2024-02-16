50e350e45fdcbed8d24543c640543cc299741c34
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-16 18:45:55 +0900

    fix(): Repository 기능 수정
    추가로 senderandrecevier값 데이터참조시켰음.

--- src/main/java/com/usg/chat/adapter/out/persistence/entity/ChatRoom/ChatRoomRepository.java
+++ src/main/java/com/usg/chat/adapter/out/persistence/entity/ChatRoom/ChatRoomRepository.java
@@ -1,7 +1,14 @@
 package com.usg.chat.adapter.out.persistence.entity.ChatRoom;
 
-import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomEntity;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, Long> {
+
+    @Query(value = "select cr from ChatRoomEntity cr" +
+            " where cr.senderAndReceiver = :senderAndReceiver")
+    ChatRoomEntity findSenderAndReceiver(@Param("senderAndReceiver") String senderAndReceiver);
 }
