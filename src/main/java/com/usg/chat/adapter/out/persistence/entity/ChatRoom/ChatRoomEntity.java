14d8483ad94c10c1fea6f43e5983a2cf7ef96ed4
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-16 18:43:57 +0900

    fix(): Entity 추가
    Setter 제거

--- src/main/java/com/usg/chat/adapter/out/persistence/entity/ChatRoom/ChatRoomEntity.java
+++ src/main/java/com/usg/chat/adapter/out/persistence/entity/ChatRoom/ChatRoomEntity.java
@@ -1,21 +1,21 @@
 package com.usg.chat.adapter.out.persistence.entity.ChatRoom;
 
+import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 @Entity
 @Getter
-@Setter
 @NoArgsConstructor
 public class ChatRoomEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ChatRoom_id")
     private Long id;
     private String senderAndReceiver;
+
+    @Builder
+    public ChatRoomEntity(String senderAndReceiver){
+        this.senderAndReceiver = senderAndReceiver;
+    }
 }
