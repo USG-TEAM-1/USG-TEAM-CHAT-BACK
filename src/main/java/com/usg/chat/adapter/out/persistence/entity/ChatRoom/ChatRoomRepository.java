13bb11ddfc3cbe924e361d53d627a3ad48e97f0d
nodap22 <sanha0046@naver.com>
2024-02-16 12:42:58 +0900

    feat(): import 추가
    ChatRoomEntity파일 <- import 추가

--- src/main/java/com/usg/chat/adapter/out/persistence/entity/ChatRoom/ChatRoomRepository.java
+++ src/main/java/com/usg/chat/adapter/out/persistence/entity/ChatRoom/ChatRoomRepository.java
@@ -1,7 +1,7 @@
 package com.usg.chat.adapter.out.persistence.entity.ChatRoom;
 
+import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoomEntity, Long> {
-
 }
