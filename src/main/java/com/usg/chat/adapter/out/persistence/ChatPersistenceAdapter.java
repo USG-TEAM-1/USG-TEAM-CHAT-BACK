5a87bcbfed54187c77ae436836726a37ca4114e7
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-19 01:27:46 +0900

    fix(): Adapater 채팅방 찾는 것 추가
    Repository를 이용하여 채팅방을 찾게 해줌.

--- src/main/java/com/usg/chat/adapter/out/persistence/ChatPersistenceAdapter.java
+++ src/main/java/com/usg/chat/adapter/out/persistence/ChatPersistenceAdapter.java
@@ -2,6 +2,8 @@ package com.usg.chat.adapter.out.persistence;
 
 import com.usg.chat.adapter.out.persistence.entity.Chat.ChatEntity;
 import com.usg.chat.adapter.out.persistence.entity.Chat.ChatRepository;
+import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomEntity;
+import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomRepository;
 import com.usg.chat.application.port.out.ChatPersistencePort;
 import com.usg.chat.domain.Chat;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +17,22 @@ import java.util.stream.Collectors;
 public class ChatPersistenceAdapter implements ChatPersistencePort {
 
     private final ChatRepository chatRepository;
+    private final ChatRoomRepository chatRoomRepository;
 
     @Override
     public Long saveMessage(Chat chat){
+        //채팅방을 찾음.
+        ChatRoomEntity chatRoomEntity = chatRoomRepository.findById(chat.getChatRoomId()).orElseThrow(
+                () -> new IllegalAccessError("채팅방을 찾을 수 없습니다.")
+        );
+
         ChatEntity chatEntity = ChatEntity
                 .builder()
                 .message(chat.getMessage())
                 .senderId(chat.getSenderId())
                 .receiverId(chat.getReceiverId())
-                //.chatroom()
                 .timestamp(chat.getTimestamp())
+                .chatRoomId(chatRoomEntity)
                 .build();
 
         ChatEntity savedChatEntity = chatRepository.save(chatEntity);
@@ -33,7 +41,7 @@ public class ChatPersistenceAdapter implements ChatPersistencePort {
     }
 
     @Override
-    public List<Chat> getMessages(String senderId, String receiverId) {
+    public List<Chat> getMessages(Long senderId, Long receiverId) {
         List<ChatEntity> chatEntities = chatRepository.getMessages(senderId,receiverId);
         return chatEntities.stream().map(entity -> Chat.builder()
                 .message(entity.getMessage())
