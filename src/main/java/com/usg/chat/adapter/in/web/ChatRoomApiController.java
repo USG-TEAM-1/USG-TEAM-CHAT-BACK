74ad3fe93c44bba11f34336127af40bf991cc90a
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-19 16:12:41 +0900

    fix(): API 최적화 진행
    req는 opponentId만 받아야함. 그래서 수정.

--- src/main/java/com/usg/chat/adapter/in/web/ChatRoomApiController.java
+++ src/main/java/com/usg/chat/adapter/in/web/ChatRoomApiController.java
@@ -2,37 +2,54 @@ package com.usg.chat.adapter.in.web;
 
 import com.usg.chat.adapter.in.web.dto.ChatRoomReq;
 import com.usg.chat.adapter.in.web.dto.ChatRoomRes;
+import com.usg.chat.adapter.in.web.dto.Result;
+import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomEntity;
+import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomRepository;
 import com.usg.chat.application.port.in.ChatRoom.ChatRoomUseCase;
 import com.usg.chat.application.port.in.ChatRoom.ChatRoomCommand;
-import com.usg.chat.domain.ChatRoom;
+import com.usg.chat.util.SortedStringEditor;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@Slf4j
 @RestController
+@AllArgsConstructor
 public class ChatRoomApiController {
     private final ChatRoomUseCase chatRoomUseCase;
+    private final ChatRoomRepository chatRoomRepository;
 
-    public ChatRoomApiController(ChatRoomUseCase chatRoomUseCase) {
-        this.chatRoomUseCase = chatRoomUseCase;
-    }
-//채팅방 생성 API
+    //채팅방 생성 API
     @PostMapping("/chat-rooms")
-    public ResponseEntity<String> createChatRoom(@RequestBody ChatRoomCommand command) {
-        Long senderId = command.getSenderId();
-        Long receiverId = command.getReceiverId();
-        // 작은 숫자부터 앞으로 오게함
-        String senderAndReceiver = (senderId < receiverId) ? senderId + "_" + receiverId : receiverId + "_" + senderId;
+    public ResponseEntity<Result> createChatRoom(@RequestBody ChatRoomReq request) {
+        Long senderId = 1L;
+        Long opponentId = request.getOpponentId();
 
-        if(chatRoomUseCase.existsBySenderAndReceiver(senderAndReceiver)){
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        // 채팅방 인원 찾기
+        String senderAndReceiver = SortedStringEditor.createSortedString(senderId, opponentId);
+        ChatRoomEntity findChatRoom = chatRoomRepository.findBySenderAndReceiver(senderAndReceiver);
+
+        // 채팅방이 없을 경우 생성
+        if (findChatRoom == null) {
+            ChatRoomCommand command = ChatRoomCommand.builder()
+                    .senderId(senderId)
+                    .receiverId(opponentId)
+                    .build();
+            chatRoomUseCase.createChatRoom(command);
+            findChatRoom = chatRoomRepository.findBySenderAndReceiver(senderAndReceiver);
         }
 
-        chatRoomUseCase.createChatRoom(senderAndReceiver);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        // 채팅방이 있을 경우 응답 생성
+        ChatRoomRes response = ChatRoomRes.builder()
+                .chatRoomId(findChatRoom.getRoomId())
+                .build();
+
+        return ResponseEntity.ok(new Result(response, "채팅방을 생성하였습니다."));
     }
+
+
     //채팅방 조회 API
     @GetMapping("/chat-rooms/id/{senderAndReceiver}")
     public ResponseEntity<ChatRoomRes> getChatRoomId(@PathVariable("senderAndReceiver") String senderAndReceiver) {
@@ -41,10 +58,8 @@ public class ChatRoomApiController {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
         ChatRoomRes chatRoomRes = new ChatRoomRes();
-        chatRoomRes.setRoomId(chatRoomId);
+        chatRoomRes.setChatRoomId(chatRoomId);
         return ResponseEntity.ok(chatRoomRes);
     }
-
-
 }
 
