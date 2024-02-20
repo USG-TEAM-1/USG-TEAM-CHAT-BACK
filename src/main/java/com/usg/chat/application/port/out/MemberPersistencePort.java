b20f88fa67aacb97096d3c85544b4fc9c40f35b8
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-20 23:10:51 +0900

    feat(): MemberPersistencePort 구현
    멤버 사용을 위한 포트

---
+++ src/main/java/com/usg/chat/application/port/out/MemberPersistencePort.java
@@ -0,0 +1,9 @@
+package com.usg.chat.application.port.out;
+
+import com.usg.chat.domain.Member;
+
+public interface MemberPersistencePort {
+
+    void saveMember(Member member);
+    String getNicknameByEmail(String email);
+}
