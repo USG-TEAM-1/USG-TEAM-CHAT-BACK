e5ed5802ad6f73f833bf563e3dbe1581033fd786
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-20 15:41:24 +0900

    feat(): Domain에 새로운 chatRoomId 추가

--- src/main/java/com/usg/chat/domain/ChatRoom.java
+++ src/main/java/com/usg/chat/domain/ChatRoom.java
@@ -3,14 +3,18 @@ package com.usg.chat.domain;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ChatRoom {
+    private Long chatRoomId;
     private String senderAndReceiver;
 
     @Builder
-    public ChatRoom(String senderAndReceiver) {
+    public ChatRoom(String senderAndReceiver,Long chatRoomId) {
+        this.chatRoomId = chatRoomId;
         this.senderAndReceiver = senderAndReceiver;
     }
 
