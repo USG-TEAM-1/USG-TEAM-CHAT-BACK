3bfe52e951815a30a4c2d088f71f0474e96b2653
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-20 23:09:43 +0900

    feat(): KafkaListener 구현
    카프카 토픽을 읽어오기 위한 위한 memberListener 구현

---
+++ src/main/java/com/usg/chat/adapter/in/kafka/MemberKafkaListener.java
@@ -0,0 +1,36 @@
+package com.usg.chat.adapter.in.kafka;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.usg.chat.application.port.in.Member.MemberWriteCommand;
+import com.usg.chat.application.port.in.Member.MemberWriteUseCase;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MemberKafkaListener {
+
+    private final MemberWriteUseCase memberWriteUseCase;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @KafkaListener(topics = "member", groupId = "chat")
+    public void memberListener(@Payload String kafka) throws JsonProcessingException {
+        MemberPublishDTO memberPublishDTO = objectMapper.readValue(kafka, MemberPublishDTO.class);
+        MemberWriteCommand memberWriteCommand = publishToCommand(memberPublishDTO);
+
+        memberWriteUseCase.memberWrite(memberWriteCommand);
+    }
+
+    private MemberWriteCommand publishToCommand(MemberPublishDTO memberPublishDTO) {
+        return MemberWriteCommand
+                .builder()
+                .email(memberPublishDTO.getEmail())
+                .nickname(memberPublishDTO.getNickname())
+                .build();
+    }
+}
