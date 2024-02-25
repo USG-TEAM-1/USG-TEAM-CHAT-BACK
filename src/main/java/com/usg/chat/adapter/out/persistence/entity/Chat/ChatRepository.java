fa1ef8591d3acf562bacaafcf50be17ca4d1c0ef
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-25 20:14:25 +0900

    feat(): ChatRepository 채팅방으로 receiverId 찾기추가
    채팅방chatRoomId로 채팅방 receiverId를 찾는 로직 생성

--- src/main/java/com/usg/chat/adapter/out/persistence/entity/Chat/ChatRepository.java
+++ src/main/java/com/usg/chat/adapter/out/persistence/entity/Chat/ChatRepository.java
@@ -1,6 +1,5 @@
 package com.usg.chat.adapter.out.persistence.entity.Chat;
 
-import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomEntity;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +11,7 @@ public interface ChatRepository extends JpaRepository<ChatEntity, Long> {
             "(c.senderId = :senderId AND c.receiverId = :receiverId) OR " +
             "(c.senderId = :receiverId AND c.receiverId = :senderId) " + "ORDER BY c.timestamp DESC")     //시간 비교 추가해야함. ORDER BY c.createdAt DES
     List<ChatEntity> getMessages(@Param("senderId") Long senderId, @Param("receiverId") Long receiverId);
+
+    @Query("SELECT c.receiverId FROM ChatEntity c WHERE c.chatRoomId.RoomId = :chatRoomId")
+    Long findReceiverIdByChatRoomId(@Param("chatRoomId") Long chatRoomId);
 }
