848b91d02cd9790a006c6773bb0ce5aa009b00fa
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-25 20:18:49 +0900

    feat(): 채팅방 받아오는 로직 수정

--- src/main/java/com/usg/chat/application/port/in/ChatRoom/GetChatRoomsUseCase.java
+++ src/main/java/com/usg/chat/application/port/in/ChatRoom/GetChatRoomsUseCase.java
@@ -3,5 +3,5 @@ package com.usg.chat.application.port.in.ChatRoom;
 import java.util.List;
 
 public interface GetChatRoomsUseCase {
-    List<GetChatRoomsRes> findChatRooms(Long memberId);
+    List<GetChatRoomsRes> findChatRooms(String email);
 }
