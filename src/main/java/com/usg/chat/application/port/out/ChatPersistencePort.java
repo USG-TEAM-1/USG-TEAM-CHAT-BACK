8645b32024e4a680509e28d7e58b6fcd18a6c37e
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-25 20:13:19 +0900

    fix(): 최적화 작업

--- src/main/java/com/usg/chat/application/port/out/ChatPersistencePort.java
+++ src/main/java/com/usg/chat/application/port/out/ChatPersistencePort.java
@@ -1,6 +1,7 @@
 package com.usg.chat.application.port.out;
 
 
+import com.usg.chat.adapter.out.persistence.entity.Chat.ChatEntity;
 import com.usg.chat.domain.Chat;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface ChatPersistencePort {
     Long saveMessage(Chat chat);
     List<Chat> getMessages(Long senderId, Long receiverId);
+
 }
