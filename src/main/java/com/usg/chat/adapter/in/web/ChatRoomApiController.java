cbd2a8159bbd93a0d2e34474af4e7c1a2db072ba
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-25 20:14:55 +0900

    fix(): 채팅방 컨트롤러 Req 수정
    req로 로그인 한 회원 및 전송자 이메일로 수정함.

--- src/main/java/com/usg/chat/adapter/in/web/ChatRoomApiController.java
+++ src/main/java/com/usg/chat/adapter/in/web/ChatRoomApiController.java
@@ -3,14 +3,13 @@ package com.usg.chat.adapter.in.web;
 import com.usg.chat.adapter.in.web.dto.ChatRoomReq;
 import com.usg.chat.adapter.in.web.dto.ChatRoomRes;
 import com.usg.chat.adapter.in.web.dto.Result;
+import com.usg.chat.adapter.in.web.token.MemberEmailGetter;
 import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomEntity;
 import com.usg.chat.adapter.out.persistence.entity.ChatRoom.ChatRoomRepository;
-import com.usg.chat.application.port.in.ChatRoom.ChatRoomUseCase;
-import com.usg.chat.application.port.in.ChatRoom.ChatRoomCommand;
-import com.usg.chat.application.port.in.ChatRoom.GetChatRoomsRes;
-import com.usg.chat.application.port.in.ChatRoom.GetChatRoomsUseCase;
+import com.usg.chat.application.port.in.ChatRoom.*;
 import com.usg.chat.util.SortedStringEditor;
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -23,32 +22,36 @@ import java.util.List;
 @AllArgsConstructor
 public class ChatRoomApiController {
     private final ChatRoomUseCase chatRoomUseCase;
-    private final ChatRoomRepository chatRoomRepository;
+    private final FindChatRoomUseCase findChatRoomUseCase;
     private final GetChatRoomsUseCase getChatRoomsUseCase;
+    private final MemberEmailGetter memberEmailGetter;
 
     //채팅방 생성 API
     @PostMapping("/chat-rooms")
-    public ResponseEntity<Result> createChatRoom(@RequestBody ChatRoomReq request) {
-        Long loginId = 1L;
-        Long opponentId = request.getOpponentId();
+    public ResponseEntity<Result> createChatRoom(@RequestBody ChatRoomReq request, HttpServletRequest servletRequest) {
+        String senderEmail = memberEmailGetter.getMemberEmail(servletRequest.getHeader("Authorization"));
+        String opponentEmail = request.getOpponentEmail();
 
         // 채팅방 인원 찾기
-        String senderAndReceiver = SortedStringEditor.createSortedString(loginId, opponentId);
-        ChatRoomEntity findChatRoom = chatRoomRepository.findBySenderAndReceiver(senderAndReceiver);
+        FindChatRoomCommand findChatRoomCommand = FindChatRoomCommand.builder()
+                .senderEmail(senderEmail)
+                .receiverEmail(opponentEmail)
+                .build();
+
+        Long findChatRoom = findChatRoomUseCase.FindChatRoom(findChatRoomCommand);
 
         // 채팅방이 없을 경우 생성
         if (findChatRoom == null) {
             ChatRoomCommand command = ChatRoomCommand.builder()
-                    .senderId(loginId)
-                    .receiverId(opponentId)
+                    .senderEmail(senderEmail)
+                    .receiverEmail(opponentEmail)
                     .build();
             chatRoomUseCase.createChatRoom(command);
-            findChatRoom = chatRoomRepository.findBySenderAndReceiver(senderAndReceiver);
         }
 
         // 채팅방이 있을 경우 응답 생성
         ChatRoomRes response = ChatRoomRes.builder()
-                .chatRoomId(findChatRoom.getRoomId())
+                .chatRoomId(findChatRoom)
                 .build();
 
         return ResponseEntity.ok(new Result(response, "채팅방을 생성하였습니다."));
@@ -57,9 +60,11 @@ public class ChatRoomApiController {
     // 채팅방 조회 API
     @GetMapping("/api/chat-rooms/")
     @Operation(summary = "채팅방 목록 조회")
-    public ResponseEntity<Result<List<GetChatRoomsRes>>> getChatRooms(@RequestParam Long memberId) {
+    public ResponseEntity<Result<List<GetChatRoomsRes>>> getChatRooms(HttpServletRequest servletRequest) {
+
+        String email = memberEmailGetter.getMemberEmail(servletRequest.getHeader("Authorization"));
         // ChatRoomEntity 대신 ChatRoomRes를 사용하여 응답 생성
-        List<GetChatRoomsRes> chatRoomResList = getChatRoomsUseCase.findChatRooms(memberId);
+        List<GetChatRoomsRes> chatRoomResList = getChatRoomsUseCase.findChatRooms(email);
         return ResponseEntity.ok(new Result<>(chatRoomResList, "채팅방 목록을 찾았습니다."));
     }
 }
