1bb372bbb8844e179ceba614d945623707d03614
Namsoo315 <hyunsoo3315@gmail.com>
2024-02-29 13:55:12 +0900

    fix(): GetMessageService 수정

--- src/main/java/com/usg/chat/application/service/GetMessageService.java
+++ src/main/java/com/usg/chat/application/service/GetMessageService.java
@@ -1,5 +1,6 @@
 package com.usg.chat.application.service;
 
+import com.usg.chat.application.port.in.Chat.GetMessageHistoryRes;
 import com.usg.chat.application.port.in.Chat.GetMessageHistoryUseCase;
 import com.usg.chat.application.port.out.ChatPersistencePort;
 import com.usg.chat.application.port.out.MemberPersistencePort;
@@ -10,19 +11,31 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
-@RequiredArgsConstructor
 @Slf4j
-@Transactional
+@RequiredArgsConstructor
 public class GetMessageService implements GetMessageHistoryUseCase {
-    private final ChatPersistencePort chatPersistencePort;
+
     private final MemberPersistencePort memberPersistencePort;
+    private final ChatPersistencePort chatPersistencePort;
+
     @Override
     @Transactional
-    public List<Chat> getMessages(String senderEmail, String receiverEmail){
+    public List<GetMessageHistoryRes> getMessages(String senderEmail, String receiverEmail) {
         Long senderId = memberPersistencePort.getIdByEmail(senderEmail);
         Long receiverId = memberPersistencePort.getIdByEmail(receiverEmail);
-        return chatPersistencePort.getMessages(senderId, receiverId);
+        List<Chat> chatMessages = chatPersistencePort.getMessages(senderId, receiverId);
+
+        return chatMessages.stream()
+                .map(chatMessage -> GetMessageHistoryRes.builder()
+                        .message(chatMessage.getMessage())
+                        .senderEmail(senderEmail)
+                        .receiverEmail(receiverEmail)
+                        .timestamp(chatMessage.getTimestamp())
+                        .chatRoomId(chatMessage.getChatRoomId())
+                        .build())
+                .collect(Collectors.toList());
     }
 }
